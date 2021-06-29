75b6c7de35682cc1073b6c0c26a590e01dc83803
Rahul Gautam Singh <rahul_ug@cse.nits.ac.in>
2021-06-29 10:57:57 +0545

    added filters

--- app.js
+++ app.js
@@ -1,36 +1,63 @@
-const express = require("express")
+const express = require("express");
 
-const app = express()
+const app = express();
 const mongoose = require("mongoose");
-const { addPlace, findPlace } = require("./controllers/placeController");
+const {
+  addPlace,
+  findPlace,
+  listPlaces,
+} = require("./controllers/placeController");
 app.use(express.json());
 
-
 mongoose.connect("mongodb://127.0.0.1:27017/placedb", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-  });
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
 
-app.get("/",(req,res)=>{
-    res.json({"message":"Good job!"})
-})
-app.post("/add",async(req,res)=>{
-    let result = await addPlace(req.body)
-    if(result.status){
-        res.json({place:result.obj})
-    }else{
-        res.send(400).json({"message":result.message})
-    }
-})
-app.get("/getplace",async(req,res)=>{
-let result = await findPlace(req.body.slug)
-if(result.status){
-    res.json({place:result.obj})
-}else{
-    res.send(400).json({"message":result.message})
-}
-})
+app.get("/", (req, res) => {
+  res.json({ message: "Good job!" });
+});
+
+
+app.post("/add", async (req, res) => {
+  let result = await addPlace(req.body);
+  if (result.status) {
+    res.json({ place: result.obj });
+  } else {
+    res.send(400).json({ message: result.message });
+  }
+});
+
+
+app.get("/getplace", async (req, res) => {
+  let result = await findPlace(req.body.slug);
+  if (result.status) {
+    res.json({ place: result.obj });
+  } else {
+    res.send(400).json({ message: result.message });
+  }
+});
+
+
+app.get("/place", async (req, res) => {
+  let filters = req.query;
+  let keys = Object.entries(filters);
+  console.log(keys);
+  keys.forEach((elem) => {
+  
+      filters[elem[0]] = filters[elem[0]].split(",");
+  });
+console.log(filters)
+  let result = await listPlaces(filters);
+  if (result.status) {
+    res.json({ place: result.obj });
+  } else {
+    res.json({ message: result.message });
+  }
+});
+
+
 
 const PORT = 3300;
 app.listen(PORT, () => {
--- controllers/placeController.js
+++ controllers/placeController.js
@@ -22,12 +22,41 @@ const findPlace = async (placeSlug) => {
   let place = await Place.findOne({ slug: placeSlug });
   if (place === null) {
     return { staus: false, message: "No such place exists" };
-  }
+  } else return { status: true, obj: place };
+};
+const listPlaces = async (placeObj) => {
+  if (placeObj.name === undefined) placeObj["name"] = [];
+  if (placeObj.state === undefined) placeObj["state"] = [];
+  if (placeObj.country === undefined) placeObj["country"] = [];
 
- else  return { status: true, obj: place };
+  let places = await Place.find({
+    $or: [
+      { name: { $in: placeObj.name } },
+      { state: { $in: placeObj.state } },
+      { country: { $in: placeObj.country } },
+    ],
+  });
+
+  //   if (placeObj.state !== undefined) {
+  //     let arr = await Place.find({ state: { $in: placeObj.state } });
+  //     if (arr.length > 0) {
+  //       arr.forEach((elem) => places.add(elem));
+  //     }
+  //   }
+  //   if (placeObj.country !== undefined) {
+  //     let arr = await Place.find({ country: { $in: placeObj.country } });
+  //     if (arr.length > 0) {
+  //       arr.forEach((elem) => places.add(elem));
+  //     }
+  //   }
+  console.log(places);
+  if (places.length === 0) {
+    return { staus: false, message: "No similar places exist." };
+  } else return { status: true, obj: places };
 };
 
 module.exports = {
   addPlace,
   findPlace,
+  listPlaces,
 };
